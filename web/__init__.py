890f6efb8de7e59a6ba432752522cca24a71bdaa
kanjicool <kanjicool2077@gmail.com>
2024-02-10 18:25:49 +0700

    Creating Models & Datebase

--- web/__init__.py
+++ web/__init__.py
@@ -1,8 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = 'database.sqlite3'
+
+def create_datebase():
+    db.create_all()
+    print('Database Created')
+
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "kanjicool"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)
+
 
     from .views import views
     from .auth import auth
@@ -11,5 +24,12 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")  # localhost:5000/about-us
     app.register_blueprint(auth, url_prefix="/auth")  # localhost:5000/auth/change-password
     app.register_blueprint(admin, url_prefix="/")
+
+    with app.app_context():
+        create_datebase()
+        
+
+
+
 
     return app
