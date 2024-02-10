4d4d84a66c1bf0055a0580d2ac1f252d1242091e
kanjicool <kanjicool2077@gmail.com>
2024-02-11 02:51:52 +0700

    Mug Hot

--- web/auth.py
+++ web/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, flash, redirect
-from .forms import LoginForm, SignUpForm
+from .forms import LoginForm, SignUpForm, PasswordChangeForm
 from .models import Customer
 from . import db
 from flask_login import login_user, login_required, logout_user
