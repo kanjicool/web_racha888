bb88ce7c605fefe109e4ab8d3db2c009376e81ef
kanjicool <kanjicool2077@gmail.com>
2024-02-11 02:08:51 +0700

    auth add route sing up loginlogout profile

--- web/__init__.py
+++ web/__init__.py
@@ -32,14 +32,12 @@ def create_app():
     from .models import Customer, Cart, Product, Order
 
     app.register_blueprint(views, url_prefix="/")  # localhost:5000/about-us
-    app.register_blueprint(auth, url_prefix="/auth")  # localhost:5000/auth/change-password
+    app.register_blueprint(auth, url_prefix="/")  # localhost:5000/auth/change-password
     app.register_blueprint(admin, url_prefix="/")
 
-    with app.app_context():
-        create_database()
+    # with app.app_context():
+    #     create_database()
         
-
-
 
 
     return app
