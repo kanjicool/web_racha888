cdee60242804773a3fb3b122d9f2ee3f7048a47d
kanjicool <kanjicool2077@gmail.com>
2024-02-11 01:42:20 +0700

    add /about

--- web/views.py
+++ web/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint("views", __name__)
 
@@ -6,3 +6,11 @@ views = Blueprint("views", __name__)
 @views.route("/")
 def home():
     return render_template('home.html')
+
+@views.route("/about")
+def about():
+    return render_template('about.html')
+
+@views.route("/contact")
+def contact():
+    return render_template('contact.html')
